9086b95ed81b699ff7ed5fc8be2614e0e9a1a176
Jiang <20042978@qq.com>
2025-08-11 15:30:41 +0800

    恢复页面

--- pages/权限管理人员名单/权限管理人员名单.js
+++ pages/权限管理人员名单/权限管理人员名单.js
@@ -17,6 +17,45 @@ Page({
     console.log('=== 权限管理人员名单页面显示 ===');
   },
 
+  onProjectQuery() {
+    console.log('点击项目查询权限人员按钮');
+    // 跳转到项目查询权限人员页面
+    wx.navigateTo({
+      url: '/pages/项目查询权限人员/项目查询权限人员',
+      success: function() {
+        console.log('成功跳转到项目查询权限人员页面');
+      },
+      fail: function(err) {
+        console.error('跳转失败:', err);
+        wx.showToast({
+          title: '页面跳转失败',
+          icon: 'none',
+          duration: 2000
+        });
+      }
+    });
+  },
+  
+  onProjectModify() {
+    console.log('点击项目修改权限人员按钮');
+    // 跳转到项目修改权限人员页面
+    wx.navigateTo({
+      url: '/pages/项目修改权限人员/项目修改权限人员',
+      success: function() {
+        console.log('成功跳转到项目修改权限人员页面');
+      },
+      fail: function(err) {
+        console.error('跳转失败:', err);
+        wx.showToast({
+          title: '页面跳转失败',
+          icon: 'none',
+          duration: 2000
+        });
+      }
+    });
+  },
+
+
 
 
   onPolicyModify() {
